b61b49a4aa8af0451bea0da2cc7b1d87e0ce8141
mdmurshidkhangit config --global user .email <mmd722584@gmail.com>
2026-03-13 15:53:35 +0530

    update

---
+++ BOM.js
@@ -0,0 +1,56 @@
+function murshid(){
+    
+    var myname= "murshid"
+
+    console.log(myname)
+
+    console.log("abhishek");
+    console.log("faiz"):
+
+
+
+         var a= 5
+         var  b=8
+
+         console.log( a+b);
+
+
+}
+
+murshid()  //call the function
+
+
+//============day 2=============
+
+function Xyz(){
+
+    //typr coercion
+
+    var a= undefined
+    var b= '5'
+
+    // 1st priority string
+    //2nd priority number
+
+    console.log(a+b) //
+    }
+    function BOM(){
+
+        // alert(), prompt()
+        // var a= 5
+        // console.log(a);
+
+
+          // alert(a)
+
+          var myname = prompt("enter your name")
+          // alert ("welcome" + myname)
+
+
+
+    }
+    function BOM(){
+        var a =5
+        var b= 6
+        alert(a+b)
+    }

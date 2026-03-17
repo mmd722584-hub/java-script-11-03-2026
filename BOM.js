94a4854cb819e12dd91e4b43d4fe360229e7a739
mdmurshidkhangit config --global user .email <mmd722584@gmail.com>
2026-03-18 03:30:54 +0530

    java script

--- BOM.js
+++ BOM.js
@@ -5,7 +5,7 @@ function murshid(){
     console.log(myname)
 
     console.log("abhishek");
-    console.log("faiz"):
+    console.log("faiz")
 
 
 
@@ -26,8 +26,8 @@ function Xyz(){
 
     //typr coercion
 
-    var a= undefined
-    var b= '5'
+    // var a= undefined
+    // var b= '5'
 
     // 1st priority string
     //2nd priority number
@@ -43,14 +43,133 @@ function Xyz(){
 
           // alert(a)
 
-          var myname = prompt("enter your name")
-          // alert ("welcome" + myname)
+        //   var myname = prompt("enter your name")
+        //   alert ("welcome" + myname)
 
 
 
     }
-    function BOM(){
-        var a =5
-        var b= 6
-        alert(a+b)
+
+    
+    // 
+    
+    function sid(){
+        }
+        //  var a= prompt("enter your number1")
+        //  var b= number(prompt("enter your number2"))
+        //  alert(number(a)+b) //'2'+'5'
+
+        //  number to string
+
+        //  parseint() , parsefloat() , number
+
+
+
+
+        //  if(condition){
+
+        //  }
+            //     // statement()
+
+            // else{
+            //      // statement if false
+         
+         
+
+    //         var age = prompt("enter you age")
+         
+    //          if (age >= 18){
+
+    //             alert("you cant vote")
+    //          }
+    //             else{
+    //                 alert("you cant")
+    //             }
+        
+            
+        
+
+
+
+
+    
+    // sid()
+    function murshid(){
+
     }
+//        let num = 1;
+
+// if (num % 2 == 0) {
+
+//   alert("Even number");
+
+// } else {
+
+//   alert("Odd number");
+
+// }
+
+       //====== mark grade ======//
+
+// let marks = prompt("Enter your marks");
+
+// if (marks >= 0 && marks <= 33) {
+//   alert("Grade D");
+// } 
+// else if (marks > 33 && marks <= 60) {
+//   alert("Grade B");
+// } 
+// else if (marks > 60 && marks <= 100) {
+//   alert("Grade A");
+// } 
+// else {
+//   alert("Invalid Marks");
+// }
+// murshid()
+
+
+// document.write("Hello cybrom");
+// document.write("Hello cybrom");
+// document.write("Hello cybrom);
+// document.write("Hello cybrom");
+// document.write("Hello cybrom");
+
+ //======for loop======//
+
+
+// for (let i = 1; i <= 5; i++) {
+//     document.write("Hello cybrom <br>");
+
+//   }
+
+// for(let i=1; i<=10; i++){
+//     document.write(i + "<br>");
+//   }
+
+
+// for(let i=1; i<=20; i++){
+//     if(i % 2 == 0){
+//       document.write(i + " " );
+//     }
+//   }
+
+
+// let sum = 0;
+
+// for (let i = 1; i <= 10; i++) {
+//   sum = sum + i;
+// }
+
+// document.write(sum);
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(5 * i);
+//   }
+
+let sum = 10;
+
+for (let i = 1; i <= 10; i++) {
+  sum = sum + i;
+}
+
+console.log(sum);
---
+++ BOM2.js
@@ -0,0 +1,35 @@
+function khan(){
+    let arr = ["A" ,"B" ,"C" ,"D" ,"E"];
+    
+    arr.push("F" ,"G")   // add "f","g"
+
+    console.log(arr)
+}
+khan()
+function abhi(){
+    let arr = ["A" ,"B" ,"C" ,"D" ,"E"];
+    
+    arr.pop()   //E hatega
+    arr.pop()   //d hatega 
+
+    console.log(arr)
+}
+abhi()
+
+function nit(){
+    let arr = ["A" ,"B" ,"C" ,"D" ,"E"];
+    
+    arr.shift()
+
+    console.log(arr)
+}
+nit()
+
+function murshidkhan(){
+    let arr = ["A" ,"B" ,"C" ,"D" ,"E"];
+    
+    arr.unshift("B")
+
+    console.log(arr)
+}
+murshidkhan()

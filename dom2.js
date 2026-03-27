271c2d2d53edddb5261307761121bdf4f8622b1e
mdmurshidkhangit config --global user .email <mmd722584@gmail.com>
2026-03-27 21:08:24 +0530

    update

--- dom2.js
+++ dom2.js
@@ -53,3 +53,90 @@
     }
 
 
+
+
+
+
+
+    // form
+
+
+    let validate = ()=>{
+    let nameval= document.querySelector('#name').value.trim()
+
+     let errname =document.querySelector("#errname")
+
+     if(nameval==''){
+        errname.innerHTML= "Please enter Name"
+
+        return false
+     }
+
+    
+        let num1 =document.querySelector("#num").value.trim()
+        let errnum=document.querySelector("#errnum")
+
+        if (num1== ''){
+            errnum.innerHTML= "Please enter number"
+
+            return false
+        }
+
+        let mail=document.querySelector("#email").value.trim()
+        let erremail =document.querySelector("#erremail")
+
+        if (mail== ''){
+            erremail.innerHTML= "Please enter email"
+
+            return false
+        }
+
+        let pass= document.querySelector("#pass").value.trim()
+        let errpass =document.querySelector("#errpass")
+
+        if (pass== ''){
+            errpass.innerHTML= "Please enter password"
+
+            return false
+        }
+
+        let pasword =document.querySelector("#cpass").value.trim()
+        let errcpass= document.querySelector("#errcpass")
+
+        if(pasword==''){
+            errcpass.innerHTML= "Please enter confirm password"
+            return false
+        }
+
+
+
+        if (num.length !=10){
+
+            errnum.innerHTML= "Please enter 10 digits only"
+            return false 
+        }
+
+        else if ( isNaN(num))     // alphabates true
+        {
+            errnum.innerHTML="Please enter valid number"
+            return false
+        }
+        if (!(email.includes("@") && email.includes('.com'))){
+            erremail.innerHTML="Please enter valid eamil"
+            return false
+        }
+
+        if( ! (pass.match(/[1234567890]/) && 
+        pass.match(/[!@$%$#^*&]/) &&
+        pass.match(/[A-Z]/) &&
+        pass.match(/[a-z]/)) ){
+            errpass.innerHTML="Please enter strong pass"
+            return false
+        }
+    
+
+    }
+
+
+    
+    
